2a0cb4ecc28f0b28d94e3ffe9af5033e2f5ce568
duy <duync@vietnamblockchain.asia>
2026-01-07 14:40:44 +0700

    update port again

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 443,
+    port: 5555,
     allowedHosts: ['x23.i247.com'],
     proxy: {
       '/rpc': {
