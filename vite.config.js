343e1a1bc0d94e8a6705533a2b5b904c60a3b017
duy <duync@vietnamblockchain.asia>
2026-01-07 16:14:24 +0700

    update base on x21

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/dashboard/',
   server: {
     port: 5555,
-    base: '/dashboard/',
     allowedHosts: ['m1.i247.com'],
     proxy: {
       '/rpc': {
